85c1b0a399e09f105b75748bcfd4474467786c55
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-14 17:01:23 +0100

    Fix query 1

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -1,7 +1,7 @@
 -- ---------------------------------------  QUERY CON SELECT
 
 -- 1. Selezionare tutti gli studenti nati nel 1990 (160)
-SELECT surname, date_of_birth FROM `students` WHERE date_of_birth LIKE '1990%';
+SELECT surname, date_of_birth FROM `students` WHERE YEAR(date_of_birth) = 1990;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti (479)
 SELECT id,name,cfu FROM `courses` WHERE cfu > 10;
