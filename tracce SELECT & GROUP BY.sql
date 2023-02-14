ab86593457413981ea0e887021ab2792dd164157
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-14 17:44:41 +0100

    Query GB 2

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -32,7 +32,7 @@ SELECT surname, phone FROM `teachers` WHERE phone IS NULL;
 SELECT COUNT(*) AS 'iscritti annui',YEAR(enrolment_date) FROM `students` GROUP BY YEAR(enrolment_date);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
-
+SELECT COUNT(*) as 'Uffici Insegnanti', office_address FROM `teachers` GROUP BY office_address;
 
 -- 3. Calcolare la media dei voti di ogni appello d'esame
 
