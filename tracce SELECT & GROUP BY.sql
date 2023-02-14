f7a684b49900d443c5cbd4ddf2a757f4d6dea487
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-14 17:36:56 +0100

    Query GB 1

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -29,7 +29,7 @@ SELECT surname, phone FROM `teachers` WHERE phone IS NULL;
 
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
-
+SELECT COUNT(*) AS 'iscritti annui',YEAR(enrolment_date) FROM `students` GROUP BY YEAR(enrolment_date);
 
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
 
