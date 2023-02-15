3c3c43dd70e47c1c6ec3278f2609178dd9a8b6fd
VictorGiuliano <victorgiuliano98@gmail.com>
2023-02-15 14:56:37 +0100

    Query Join 2

--- tracce SELECT & GROUP BY.sql
+++ tracce SELECT & GROUP BY.sql
@@ -46,3 +46,6 @@ SELECT COUNT(*) as 'Corsi laurea', department_id FROM `degrees` GROUP BY departm
 
 --1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
 SELECT `students`.`surname`,`students`.`name`, `degrees`.`name`AS 'Corso' FROM `degrees` JOIN `students` ON `degrees`.`id` = `students`.`degree_id` WHERE `degrees`.`name` = 'Corso di Laurea in Economia';
+
+--2.  Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
+SELECT `degrees`.`name`AS 'Corsi', `departments`.`name` AS 'Dipartimento' FROM `degrees` JOIN `departments` ON `departments`.`id` = `degrees`.`department_id` WHERE `departments`.`name` = 'Dipartimento di Neuroscienze';
